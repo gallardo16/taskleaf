badfc95a7f1412ad7bf7aced802a1265c0706baf
gallardo <aiki.hosogaya@gmail.com>
2020-08-14 17:15:41 +0900

    Add a confirmation screen before creating task and add search function

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,12 +1,20 @@
 class Task < ApplicationRecord
-#  before_validation :set_nameless_name
+  def self.ransackable_attributes(auth_object = nil)
+    %w[name created_at]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    []
+  end
+
+  before_validation :set_nameless_name
   validates :name, presence: true
   validates :name, length: { maximum: 30 }
   validate :validate_name_not_including_comma
 
   belongs_to :user
 
-  scope :recent, -> {order(crated_at: :desc) }
+  scope :recent, -> {order(created_at: :desc) }
 
   private
 
